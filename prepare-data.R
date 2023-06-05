03b1c8bf1d23ad89ed743b632590fc994cb4fff7
Nick Waters <nickp60@users.noreply.github.com>
2023-06-05 10:59:58 -0400

    Add group column to sample key

--- prepare-data.R
+++ prepare-data.R
@@ -6,15 +6,17 @@
 
 #Here we write out a key relating the NCBI accessions, sample names, and annonymous ids
 
-# read.csv("Data/ncbi_accessions.csv") %>%
-#   mutate(fid = gsub("[._]", "", toupper(sample_name))) %>%
-#   left_join(
-#     bind_rows(
-#       sample_data(mpa_phy_colitis) %>% as_tibble(),
-#       sample_data(mpa_phy_fmt) %>% as_tibble() %>% mutate(anonymized_id=as.character(MRN))
-#     ) %>%
-#       mutate(fid = gsub("[._]", "", toupper(.sample))) %>%
-#       select(fid, anonymized_id)
-#     )  %>% 
-#   select(accession, sample_name, anonymized_id) %>%
-#   distinct() %>% write.csv("Data/sample_key.csv", row.names = FALSE)
+read.csv("Data/ncbi_accessions.csv") %>%
+  mutate(fid = gsub("[._]", "", toupper(sample_name))) %>%
+  left_join(
+    bind_rows(
+      sample_data(mpa_phy_colitis) %>% as_tibble() %>%mutate(grp = "Colitis"),
+      sample_data(mpa_phy_fmt) %>% as_tibble() %>% mutate(anonymized_id=as.character(MRN), grp = "FMT")
+    ) %>%
+      mutate(fid = gsub("[._]", "", toupper(.sample))) %>%
+      select(fid, anonymized_id, grp)
+    )  %>%
+  select(accession, sample_name, anonymized_id, grp) %>%
+  mutate(dummy="Group") %>% 
+  pivot_wider(names_from = dummy, values_from = grp, values_fn = function(x){paste(x, collapse=",")}) %>% 
+  distinct() %>% write.csv("Data/sample_key.csv", row.names = FALSE)
