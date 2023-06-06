7b8581602dee358ec330a44a8451abf1c889433d
Nick Waters <nickp60@users.noreply.github.com>
2023-06-06 15:42:22 -0400

    add relevant relative dates

--- prepare-data.R
+++ prepare-data.R
@@ -15,10 +15,27 @@ read.csv("Data/ncbi_accessions.csv") %>%
       sample_data(mpa_phy_fmt) %>% as_tibble() %>% mutate(anonymized_id=as.character(MRN), grp = "FMT")
     ) %>%
       mutate(fid = gsub("[._]", "", toupper(.sample))) %>%
-      select(fid, anonymized_id, grp)
+      select(fid, anonymized_id, Comments,  grp, rel_date_colitis, dfmt1, dfmt2)
     )  %>%
-  select(accession, sample_name, anonymized_id, grp) %>%
+  select(accession, sample_name, anonymized_id, Comments, grp, rel_date_colitis, dfmt1, dfmt2) %>%
+  mutate(dfmt1 = as.character(dfmt1), dfmt2=as.character(dfmt2)) %>% 
+  group_by(sample_name) %>%
+  arrange(desc(rel_date_colitis)) %>% 
+  fill(rel_date_colitis) %>% 
+  arrange(desc(dfmt1)) %>% 
+  fill(dfmt1) %>% 
+  arrange(desc(dfmt2)) %>% 
+  fill(dfmt2) %>% 
+  arrange(desc(Comments)) %>% 
+  fill(Comments) %>% 
   mutate(`Included in Elkrief et al` = !sample_name %in% c(473, 480, 482, 499)) %>% 
   mutate(dummy="Group") %>% 
+  rename("Alternative ID"=Comments,
+         "Days relative to symptom onset"=rel_date_colitis,
+         "Days relative to first fmt" = dfmt1,
+  "Days relative to second fmt" = dfmt2 ) %>% 
+  #  rename("Day relative to FMT(s)" =drfmts) %>% 
   pivot_wider(names_from = dummy, values_from = grp, values_fn = function(x){paste(x, collapse=",")}) %>% 
-  distinct() %>% write.csv("Data/sample_key.csv", row.names = FALSE)
+  distinct() %>% 
+  arrange(Group) %>% 
+  write.csv("Data/sample_key.csv", row.names = FALSE, na = "-") 
